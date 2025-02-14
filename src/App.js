57b30185bc15d0fb668fb33df681b2baaf19e055
Shubham <shub297@gmail.com>
2025-02-14 01:03:11 -0600

    feat: Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
