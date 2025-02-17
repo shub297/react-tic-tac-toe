0065145d261dcc977316b0d5c1f5d6aa70da7a73
Shubham <shub297@gmail.com>
2025-02-16 19:38:19 -0600

    chore: simplifies board component

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -25,32 +25,28 @@ function Board({ xIsNext, squares, onPlay }: BoardProps) {
     onPlay(updatedBoard);
   }
 
-  const winner: string | null = calculateWinner(squares);
-  let status: string;
-  status = winner
-    ? "Winner: " + winner
-    : "Next Player: " + (xIsNext ? "X" : "O");
+  function renderSquare(index: number) {
+    return (
+      <Square value={squares[index]} onSquareClick={() => handleClick(index)} />
+    );
+  }
 
   return (
     <>
       <div className="board">
-        <div className="board-row">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-        </div>
+        {[0, 3, 6].map((row: number) => (
+          <div className="board-row" key={row}>
+            {renderSquare(row)}
+            {renderSquare(row + 1)}
+            {renderSquare(row + 2)}
+          </div>
+        ))}
       </div>
-      <h2>{status}</h2>
+      <h2>
+        {calculateWinner(squares)
+          ? `Winner: ${calculateWinner(squares)}`
+          : `Next Player: ${xIsNext ? "X" : "O"}`}
+      </h2>
     </>
   );
 }
@@ -66,8 +62,7 @@ function calculateWinner(squares: (string | null)[]): string | null {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
+  for (const [a, b, c] of lines) {
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
